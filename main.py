930342c0bad111e32040796a2e81ced6f4df190b
Tyler K <tylerkruer1@gmail.com>
2022-12-13 13:33:56 -0700

    quick update

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
 from os import environ
 from src import tools
+from dotenv import load_dotenv
 
 def main():
     if not os.path.exists('.env'):
         print('No .env file found')
         exit(1)
     else:
+        load_dotenv()
+        api_key = os.environ.get('API_KEY')
+        tools.loadscript(key=api_key)
         choice = input('Do you want to create or destroy proxies? (enter "C" or "D") or write current proxies to text file(enter "W")?')
         if choice == 'C' or choice == 'c':
-            tools.server_create()
+            tools.server_create(key=api_key)
         elif choice == 'D' or choice== 'd':
-            tools.server_destroy()
+            tools.server_destroy(key=api_key)
         elif choice == 'W' or choice == 'w':
-            tools.write_to_file()
+            tools.write_to_file(key=api_key)
         else:
             print('You entered your something incorrectly, please enter as specified.')
             main()
--- src/tools.py
+++ src/tools.py
@@ -2,12 +2,29 @@ import requests
 import json
 import os
 
-def server_create():
+def loadscript(key: str) -> str:
+    f = open('src/startup.txt', 'r')
+    content = f.read()
+    data = [
+        ('name', 'script'),
+        ('script', content),
+    ]
+    headers = {'API-Key': key}
+    url = 'https://api.vultr.com/v1/'
+    load_script = requests.post(url + 'startupscript/create', headers=headers, data=data)
+    #write response to file
+    f = open('src/config.json', 'w')
+    f.write(load_script.text)
+    return load_script.json()
+
+def server_create(key: str) -> str:
+    headers = {'API-Key': key}
+    url = 'https://api.vultr.com/v1/'
     return
 
-def server_destroy():
+def server_destroy(key: str) -> str:
     return
 
-def write_to_file():
+def write_to_file(key: str) -> str:
     return
 
