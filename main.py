826b5c04081f02b3df16ea2176cfa9fbb1a76cd7
Tyler K <tylerkruer1@gmail.com>
2022-12-13 14:54:57 -0700

    quick update

--- main.py
+++ main.py
@@ -2,19 +2,36 @@ import os
 from os import environ
 from src import tools
 from dotenv import load_dotenv
+import json
 
 def main():
+    load_dotenv()
     if not os.path.exists('.env'):
         print('No .env file found')
         exit(1)
     else:
-        load_dotenv()
         api_key = os.environ.get('API_KEY')
-        tools.loadscript(key=api_key)
-        choice = input('Do you want to create or destroy proxies? (enter "C" or "D") or write current proxies to text file(enter "W")?')
-        if choice == 'C' or choice == 'c':
-            tools.server_create(key=api_key)
-        elif choice == 'D' or choice== 'd':
+        # fix this here
+        choice = input('Do you want to create or destroy proxies? (enter "C" or "D") or write current proxies to text file(enter "W")? or check account info(enter "I")? create script(enter "S")? ')
+        if choice == 'I' or choice == 'i':
+            accountInfo = tools.loadAccountInfo(key=api_key)
+        elif choice == 'S' or choice == 's':
+            script = tools.createStartupScript(key=api_key)
+            print(script)
+        elif choice == 'C' or choice == 'c':
+            try:
+                tools.createStartupScript(key=api_key)
+                proxylocation = input('Enter where you want your proxies located [Miami, Dallas, Seattle, Atlanta, Chicago, LA, NY, Silicon Valley, Germany, France, UK, Amsterdam, Tokyo, Sydney]: ')
+                numproxies = input('Enter number of proxies you want to create: ')
+                #get json data from src/config.json
+                with open('src/config.json') as f:
+                    data = json.load(f)
+                    scriptid = data['v2_id']
+                    tools.create(key=api_key, location=proxylocation, proxies=numproxies, osid=scriptid)
+            except:
+                print('Error creating proxies')
+                exit(1)
+        elif choice == 'D' or choice == 'd':
             tools.server_destroy(key=api_key)
         elif choice == 'W' or choice == 'w':
             tools.write_to_file(key=api_key)
--- src/tools.py
+++ src/tools.py
@@ -1,26 +1,92 @@
 import requests
+import time
+from base64 import b64encode
 import json
 import os
 
-def loadscript(key: str) -> str:
-    f = open('src/startup.txt', 'r')
-    content = f.read()
-    data = [
-        ('name', 'script'),
-        ('script', content),
-    ]
-    headers = {'API-Key': key}
-    url = 'https://api.vultr.com/v1/'
-    load_script = requests.post(url + 'startupscript/create', headers=headers, data=data)
-    #write response to file
-    f = open('src/config.json', 'w')
-    f.write(load_script.text)
-    return load_script.json()
 
-def server_create(key: str) -> str:
+def loadAccountInfo(key: str) -> str:
+    headers = {
+        'Authorization': f'Bearer {key}',
+    }
+    response = requests.get('https://api.vultr.com/v2/account', headers=headers)
+    print(response.json())
+    return response.json()
+
+def createStartupScript(key: str) -> str:
+    headers = {
+        'Authorization': f'Bearer {key}',
+        'Content-Type': 'application/json',
+    }
+    f = open('src/startup.txt', 'r')
+    content = str(f.read())
+    base64_encoded = 'QmFzZTY0IEV4YW1wbGUgRGF0YQ=='
+    # come back to this here
+    json_data = {
+        'name': 'Squid Proxy Startup Script',
+        # double check this here
+        'type': 'pxe',
+        'script': f'{base64_encoded}',
+    }
+
+    response = requests.post('https://api.vultr.com/v2/startup-scripts', headers=headers, json=json_data)
+    f = open('src/config.json', 'w')
+    f.write(response.text)
+    return response.json()
+
+def create(key: str, location: str, proxies: str, osid: str) -> str:
     headers = {'API-Key': key}
     url = 'https://api.vultr.com/v1/'
-    return
+    create_response = requests.get(url + 'startupscript/list', headers=headers)
+    serverlist = []
+    create_data = create_response.json()
+    for subid, server in create_data.items():
+        serverlist.append(server)
+        scriptData = subid
+    if location == 'Miami':
+        locale = '39'
+    elif location == 'Dallas':
+        locale = '3'
+    elif location == 'Seattle':
+        locale = '4'
+    elif location == 'Atlanta':
+        locale = '6'
+    elif location == 'Chicago':
+        locale = '2'
+    elif location == 'LA':
+        locale = '5'
+    elif location == 'NY':
+        locale = '1'
+    elif location == 'Silicon Valley':
+        locale = '12'
+    elif location == 'Germany':
+        locale = '9'
+    elif location == 'France':
+        locale = '24'
+    elif location == 'UK':
+        locale = '8'
+    elif location == 'Amsterdam':
+        locale = '7'
+    elif location == 'Singapore':
+        locale = '40'
+    elif location == 'Tokyo':
+        locale = '25'
+    elif location == 'Sydney':
+        locale = '19'
+    else:
+        print('You entered an invalid location, please try again.')
+    data = [
+        ('DCID', locale),
+        ('VPSPLANID',"201"),
+        ('OSID',"160"),
+        ('SCRIPTID', osid),
+        ('SUBID', "test")
+    ]
+    for i in range(int(proxies)):
+        create_server = requests.post(url + 'server/create', headers=headers, data=data)
+        print(create_server.text)
+        time.sleep(1)
+    return "Successfully created proxies"
 
 def server_destroy(key: str) -> str:
     return
